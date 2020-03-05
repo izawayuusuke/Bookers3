4a596d40a0d718b803e6d23ff86cdaf76b9f1c34
izawayuusuke <yuu0502@gmail.com>
2020-03-05 06:06:19 +0000

    [Add] Home

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,27 @@ class BooksController < ApplicationController
   def show
   end
 
+  def create
+	@book = Book.new(book_params)
+	@book.user_id = current_user.id
+  	if @book.save
+  	  flash[:notice] = "You have created book successfully."
+  	  redirect_to user_path(@book)
+    else
+      @books = Book.all
+      render "users/show"
+  	end
+  end
+
   def edit
+  end
+
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
+  end
+
+  def user_params
+  	params.require(:user).permit(:name, :introduction, :profile_image_id)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,28 @@
 class UsersController < ApplicationController
   def index
+    @book = Book.new
+  	@books = Book.all
   end
 
   def show
+  	@book = Book.new
+  	@books = Book.all
+  end
+
+  def create
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  private
+
+  def book_params
+  	params.require(:book).permit(:title, :body)
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image_id)
   end
 end
