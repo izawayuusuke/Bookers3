e500002c0259cd13b6db42293065f297e58003c2
izawayuusuke <yuu0502@gmail.com>
2020-04-08 12:31:07 +0000

    Implement chat

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,9 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  has_many :chats, dependent: :destroy
+  has_many :user_rooms, dependent: :destroy
+  has_many :rooms, through: :user_rooms
   attachment :profile_image, destroy: false
 
   validates :name, presence: true, length: { in: 2..20 }
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 
   devise_for :users, controllers: {
-    sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
   root to: 'homes#home'
   get '/home/about' => "homes#about"
   get '/search' => 'users#search'
+  get 'chat/:id' => 'chats#show', as: 'chat'
   resources :books, only: [:index, :show, :edit, :create, :update, :destroy] do
     resource :favorites, only: [:create, :destroy]
     resource :book_comments, only: [:create, :destroy]
@@ -17,4 +17,5 @@ Rails.application.routes.draw do
     end
   end
   resources :relationships, only: [:create, :destroy]
+  resources :chats, only: [:create]
 end
